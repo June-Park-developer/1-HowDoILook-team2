5055b1426f69833d45f16b43e77c182f997b22ca
jbyoum <jbyoum3@gmail.com>
2025-03-11 13:59:18 +0900

    swagger 도중 임시 저장

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
+
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger/swagger-output.json" with { type: "json" };
+
 import commentRouter from "./routes/comment.js";
 import curationRouter from "./routes/curating.js";
 import imageRouter from "./routes/image.js";
@@ -14,11 +18,12 @@ app.use(express.json());
 
 app.use("/styles", styleRouter);
 app.use("/comments", commentRouter);
-
 app.use("/curations", curationRouter);
 
 app.use("/images", imageRouter);
 app.use("/download", express.static("files"));
+
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(process.env.PORT || 3000, () =>
   console.log(`Server started on ${process.env.PORT}`)
